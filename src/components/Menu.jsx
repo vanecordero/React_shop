5a6120ba273b6e15b60cabbcd91f20e0c6567023
Rosa V. Cordero Ozoria <rcordero@SERDOM.COM>
2022-04-28 15:52:20 -0400

    :sparkles: [ADD] All the components (ordero, items, products, shopping item)

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Menu.scss";
 
 export const Menu = () => {
   return (
---
+++ src/components/Order.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Order.scss";
+
+export const Order = () => {
+  return (
+    <div className="Order">
+      <p>
+        <span>03.25.21</span>
+        <span>6 articles</span>
+      </p>
+      <p>$560.00</p>
+      <img src="./icons/flechita.svg" alt="arrow" />
+    </div>
+  );
+};
